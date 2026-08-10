59cdef4086fcf7439e95b4ff2ee65ef5c23e17fe
khunfha <knomsai@gmail.com>
2026-08-10 23:07:54 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,580 @@
+const sourceGroups = [
+  {
+    folder: "heart shot",
+    title: "Girls Selfie Heart Shot Collectible Random Card",
+    files: [
+      "1-J1.png",
+      "1-J2.png",
+      "1-J3.png",
+      "1-JJ1.png",
+      "1-JJ2.png",
+      "1-JJ3.png",
+      "1-JJJ-secret.png",
+    ],
+  },
+  {
+    folder: "elements of seasons",
+    title: "Elements of Seasons Series Exclusive Photocard Set",
+    files: [
+      "2-J1.png",
+      "2-J2.png",
+      "2-J3.png",
+      "2-JJ1.png",
+      "2-JJ2.png",
+      "2-JJ3.png",
+      "2-JJJ1.png",
+      "2-JJJ2.png",
+      "2-JJJ3.png",
+    ],
+  },
+  {
+    folder: "pop-up tokyo 2026",
+    title: "GMMTV Pop-up Store in Tokyo 2026 Girls Random Card",
+    files: [
+      "3-c-j1.png",
+      "3-c-j2.png",
+      "3-c-j3.png",
+      "3-c-jj1.png",
+      "3-c-jj2.png",
+      "3-c-jj3.png",
+      "3-c-jjj-secret.png",
+    ],
+  },
+  {
+    folder: "pop-up tokyo 2026",
+    title: "GMMTV Pop-up Store in Tokyo 2026 Transparent Photoset",
+    files: [
+      "3-t-j1.png",
+      "3-t-jj1.png",
+      "3-t-jjj1.png",
+    ]
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits Collectible Card Set",
+    files: [
+      "4-f-j1.png",
+      "4-f-j2.png",
+      "4-f-j3.png",
+      "4-f-j4.png",
+      "4-f-j5.png",
+      "4-f-j6.png",
+      "4-f-jj1.png",
+      "4-f-jj2.png",
+      "4-f-jj3.png",
+      "4-f-jj4.png",
+      "4-f-jj5.png",
+      "4-f-jj6.png",
+      "4-f-jjj1.png",
+      "4-f-jjj2.png",
+      "4-f-jjj3.png",
+      "4-f-jjj4.png",
+      "4-f-jjj5.png",
+      "4-f-jjj6.png",
+      "4-f-jjj7.png",
+      "4-f-j-secret.png",
+      "4-f-jj-secret.png",
+      "4-f-jjj1-secret.png",
+      "4-f-jjj2-secret.png",
+      "4-f-jjj3-secret.png",
+    ],
+  },
+  {
+    folder: "pop-up taipei 2026",
+    title: "GMMTV Pop-up Store in Taipei 2026 Girls Collectible Random Card Happy Fam Bonds",
+    files: [
+      "5-c-j1.png",
+      "5-c-j2.png",
+      "5-c-j3.png",
+      "5-c-jj1.png",
+      "5-c-jj2.png",
+      "5-c-jj3.png",
+      "5-c-jjj-secret.png",
+    ],
+  },
+  {
+    folder: "pop-up taipei 2026",
+    title: "GMMTV Pop-up Store in Taipei 2026 Transparent Photoset",
+    files: [
+      "5-t-j1.png",
+      "5-t-jj1.png",
+      "5-t-jjj1.png",
+      "5-t-jjj2.png",
+    ],
+  },
+  {
+    folder: "pop-up taipei 2026",
+    title: "GMMTV Pop-up Store in Taipei 2026 Acrylic Magnetic Card Holder",
+    files: [
+      "5-m-jjj1.png",
+    ],
+  },
+  {
+    folder: "bb 2026",
+    title: "BLUSH BLOSSOM FAN FEST 2026 : MIDNIGHT BLOOM Collectible Random Card",
+    files: [
+      "7-c-j1.png",
+      "7-c-j2.png",
+      "7-c-j3.png",
+      "7-c-jj1.png",
+      "7-c-jj2.png",
+      "7-c-jj3.png",
+      "7-c-jjj1.png",
+      "7-c-jjj2.png",
+      "7-c-jjj-secret.png",
+    ],
+  },
+  {
+    folder: "bb 2026",
+    title: "BLUSH BLOSSOM FAN FEST 2026 : MIDNIGHT BLOOM Mini Photo set",
+    files: [
+      "7-m-j1.png",
+      "7-m-j2.png",
+      "7-m-jj1.png",
+      "7-m-jj2.png",
+      "7-m-jjj1.png",
+      "7-m-jjj2.png",
+    ],
+  },
+  {
+    folder: "jewel doll keychain",
+    title: "Jewel Doll Keychain Version 1",
+    files: [
+      "8-jewel-1.png",
+    ],
+  },
+  {
+    folder: "bb 2026",
+    title: "DVD Boxset BLUSH BLOSSOM FAN FEST 2026 : MIDNIGHT BLOOM",
+    files: [
+      "7-b-jjj1.png",
+      "7-b-jjj2.png",
+    ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits Collectible Card Set (On-Scene Edition)",
+    files: [
+      "4-o-j1.png",
+      "4-o-j2.png",
+      "4-o-j3.png",
+      "4-o-j4.png",
+      "4-o-j5.png",
+      "4-o-j6.png",
+      "4-o-jj1.png",
+      "4-o-jj2.png",
+      "4-o-jj3.png",
+      "4-o-jj4.png",
+      "4-o-jj5.png",
+      "4-o-jj6.png",
+      "4-o-jjj1.png",
+      "4-o-jjj2.png",
+      "4-o-jjj3.png",
+      "4-o-jjj4.png",
+      "4-o-jjj5.png",
+      "4-o-jjj6.png",
+      "4-o-jjj7.png",
+      "4-o-j-secret.png",
+      "4-o-jj-secret.png",
+      "4-o-jjj1-secret.png",
+      "4-o-jjj2-secret.png",
+      "4-o-jjj3-secret.png",
+    ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits Fox Neck Hugger Pillow",
+    files: [
+      "4-p-j1.png",
+      ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits Tiger Neck Hugger Pillow",
+    files: [
+      "4-p-jj1.png",
+    ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits WORK HARD PLAY HARDER Magnetic Wallet",
+    files: [
+      "4-m-j1.png",
+      ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits WORK ไร้ BALANCE Magnetic Wallet",
+    files: [
+      "4-m-jj1.png",
+    ],
+  },
+  {
+    folder: "ewb",
+    title: "Enemies with Benefits Memorial Boxset",
+    files: [
+      "4-b-j1.png",
+      "4-b-j2.png",
+      "4-b-jj1.png",
+      "4-b-jj2.png",
+      "4-b-jjj1.png",
+      "4-b-jjj2.png",
+    ],
+  },
+  {
+    folder: "all eyes here",
+    title: "Girls All Eyes Here Collectible Random Card",
+    files: [
+      "9-c-j1.png",
+      "9-c-j2.png",
+      "9-c-j3.png",
+      "9-c-jj1.png",
+      "9-c-jj2.png",
+      "9-c-jj3.png",
+      "9-c-jjj-secret.png",
+    ],
+  },
+  {
+    folder: "j space",
+    title: "J SPACE The Official Photobook",
+    files: [
+      "10-j1.png",
+      "10-j2.png",
+      "10-jj1.png",
+      "10-jj2.png",
+      "10-jjj1.png",
+      "10-jjj2.png",
+      "10-jjj3.png",
+      "10-jjj4.png",
+    ],
+  },
+  {
+    folder: "fandom characters 2026",
+    title: "Character Fest Thailand 2026 GMMTV Fandom Character Random Card",
+    files: [
+      "11-jewel-1.png",
+      "11-jewel-sr.png",
+      "11-jewel-ssr.png",
+    ],
+  },
+];
+
+const galleryGroups = document.getElementById("galleryGroups");
+const pageCount = document.getElementById("pageCount");
+const filterBar = document.getElementById("filterBar");
+const rarityFilterBar = document.getElementById("rarityFilterBar");
+const folderFilterBar = document.getElementById("folderFilterBar");
+const categoryFilterTags = ["All", "JAN", "JINGJING", "JANJINGJING", "JEWEL"];
+const rarityFilterTags = ["All", "NORMAL", "SECRET", "SUPER RARE", "SUPER SECRET RARE"];
+const filterFolders = ["All", ...sourceGroups.map((group) => group.title)];
+let activeCategoryFilters = ["All"];
+let activeRarityFilters = ["All"];
+let activeFolderFilters = ["All"];
+
+function createTemplateCard(src, alt, fileName) {
+  const card = document.createElement("button");
+  card.type = "button";
+  card.className = "template-card";
+  card.dataset.templateId = alt.replace(/[^a-zA-Z0-9]/g, "");
+
+  const img = document.createElement("img");
+  img.src = src;
+  img.alt = alt;
+  card.appendChild(img);
+
+  const lowerFileName = fileName.toLowerCase();
+  const tags = [];
+  const isSecret = /-secret(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName);
+  const isSuperSecretRare = /-ssr(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName);
+  const isSuperRare = /-sr(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName);
+
+  if (isSuperSecretRare) {
+    tags.push("SUPER SECRET RARE");
+  } else if (isSuperRare) {
+    tags.push("SUPER RARE");
+  }
+
+  let categoryTag = null;
+  if (/-jjj(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName)) {
+    categoryTag = "JANJINGJING";
+  } else if (/-jj(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName)) {
+    categoryTag = "JINGJING";
+  } else if (/-jewel(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName)) {
+    categoryTag = "JEWEL";
+  } else if (/-j(?:[\w-]*)(?:[^\w]|$)/i.test(lowerFileName)) {
+    categoryTag = "JAN";
+  }
+
+  if (categoryTag) {
+    tags.push(categoryTag);
+  }
+
+  if (isSecret) {
+    tags.push("SECRET");
+  }
+
+  const uniqueTags = [...new Set(tags)];
+
+  if (uniqueTags.length > 0) {
+    const tag = document.createElement("span");
+    tag.className = "template-tag";
+    tag.textContent = uniqueTags[0];
+    card.appendChild(tag);
+    card.dataset.tag = uniqueTags[0];
+    card.dataset.tags = uniqueTags.join(",");
+  } else {
+    card.dataset.tag = "NONE";
+    card.dataset.tags = "NONE";
+  }
+
+  if (isSecret) {
+    card.classList.add("secret");
+  }
+
+  card.addEventListener("click", () => {
+    document.querySelectorAll(".template-card").forEach((node) => node.classList.remove("active"));
+    card.classList.add("active");
+  });
+
+  return card;
+}
+
+function updateFilterButtonState(container, activeFilters) {
+  container.querySelectorAll(".filter-button").forEach((node) => node.classList.remove("active"));
+  if (activeFilters.includes("All")) {
+    const allButton = Array.from(container.children).find((node) => node.textContent === "All");
+    if (allButton) allButton.classList.add("active");
+  } else {
+    Array.from(container.children).forEach((node) => {
+      if (activeFilters.includes(node.textContent)) {
+        node.classList.add("active");
+      }
+    });
+  }
+}
+
+function createCategoryFilterButtons() {
+  filterBar.innerHTML = "";
+  categoryFilterTags.forEach((tag) => {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "filter-button";
+    button.textContent = tag;
+    button.addEventListener("click", () => {
+      if (tag === "All") {
+        activeCategoryFilters = ["All"];
+      } else {
+        const index = activeCategoryFilters.indexOf(tag);
+        if (index === -1) {
+          activeCategoryFilters.push(tag);
+        } else {
+          activeCategoryFilters.splice(index, 1);
+        }
+        if (activeCategoryFilters.length === 0) {
+          activeCategoryFilters = ["All"];
+        } else {
+          const allIndex = activeCategoryFilters.indexOf("All");
+          if (allIndex !== -1) {
+            activeCategoryFilters.splice(allIndex, 1);
+          }
+        }
+      }
+      filterCards();
+      updateFilterButtonState(filterBar, activeCategoryFilters);
+    });
+    filterBar.appendChild(button);
+  });
+
+  const clearButton = document.createElement("button");
+  clearButton.type = "button";
+  clearButton.className = "filter-button clear-button";
+  clearButton.textContent = "Clear";
+  clearButton.addEventListener("click", () => {
+    activeCategoryFilters = ["All"];
+    filterCards();
+    updateFilterButtonState(filterBar, activeCategoryFilters);
+  });
+  filterBar.appendChild(clearButton);
+}
+
+function createRarityFilterButtons() {
+  rarityFilterBar.innerHTML = "";
+  rarityFilterTags.forEach((tag) => {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "filter-button";
+    button.textContent = tag;
+    button.addEventListener("click", () => {
+      if (tag === "All") {
+        activeRarityFilters = ["All"];
+      } else {
+        const index = activeRarityFilters.indexOf(tag);
+        if (index === -1) {
+          activeRarityFilters.push(tag);
+        } else {
+          activeRarityFilters.splice(index, 1);
+        }
+        if (activeRarityFilters.length === 0) {
+          activeRarityFilters = ["All"];
+        } else {
+          const allIndex = activeRarityFilters.indexOf("All");
+          if (allIndex !== -1) {
+            activeRarityFilters.splice(allIndex, 1);
+          }
+        }
+      }
+      filterCards();
+      updateFilterButtonState(rarityFilterBar, activeRarityFilters);
+    });
+    rarityFilterBar.appendChild(button);
+  });
+
+  const clearButton = document.createElement("button");
+  clearButton.type = "button";
+  clearButton.className = "filter-button clear-button";
+  clearButton.textContent = "Clear";
+  clearButton.addEventListener("click", () => {
+    activeRarityFilters = ["All"];
+    filterCards();
+    updateFilterButtonState(rarityFilterBar, activeRarityFilters);
+  });
+  rarityFilterBar.appendChild(clearButton);
+}
+
+function createFolderFilterButtons() {
+  folderFilterBar.innerHTML = "";
+  filterFolders.forEach((folder) => {
+    const button = document.createElement("button");
+    button.type = "button";
+    button.className = "filter-button";
+    button.textContent = folder;
+    button.addEventListener("click", () => {
+      if (folder === "All") {
+        activeFolderFilters = ["All"];
+      } else {
+        const index = activeFolderFilters.indexOf(folder);
+        if (index === -1) {
+          activeFolderFilters.push(folder);
+        } else {
+          activeFolderFilters.splice(index, 1);
+        }
+        if (activeFolderFilters.length === 0) {
+          activeFolderFilters = ["All"];
+        } else {
+          const allIndex = activeFolderFilters.indexOf("All");
+          if (allIndex !== -1) {
+            activeFolderFilters.splice(allIndex, 1);
+          }
+        }
+      }
+      filterCards();
+      document.querySelectorAll("#folderFilterBar .filter-button").forEach((node) => node.classList.remove("active"));
+      if (activeFolderFilters.includes("All")) {
+        const allButton = Array.from(folderFilterBar.children).find((node) => node.textContent === "All");
+        if (allButton) allButton.classList.add("active");
+      } else {
+        Array.from(folderFilterBar.children).forEach((node) => {
+          if (activeFolderFilters.includes(node.textContent)) {
+            node.classList.add("active");
+          }
+        });
+      }
+    });
+    folderFilterBar.appendChild(button);
+  });
+
+  const clearFolderButton = document.createElement("button");
+  clearFolderButton.type = "button";
+  clearFolderButton.className = "filter-button clear-button";
+  clearFolderButton.textContent = "Clear";
+  clearFolderButton.addEventListener("click", () => {
+    activeFolderFilters = ["All"];
+    filterCards();
+    document.querySelectorAll("#folderFilterBar .filter-button").forEach((node) => node.classList.remove("active"));
+    const allButton = Array.from(folderFilterBar.children).find((node) => node.textContent === "All");
+    if (allButton) allButton.classList.add("active");
+  });
+  folderFilterBar.appendChild(clearFolderButton);
+}
+
+function updatePageCount() {
+  const visibleCards = Array.from(document.querySelectorAll(".template-card")).filter((card) => card.style.display !== "none");
+  if (pageCount) {
+    pageCount.textContent = `${visibleCards.length} photocards`;
+  }
+}
+
+function filterCards() {
+  document.querySelectorAll(".template-card").forEach((card) => {
+    const tags = card.dataset.tags ? card.dataset.tags.split(",") : [card.dataset.tag];
+    const categoryMatch = activeCategoryFilters.includes("All") || tags.some((tag) => activeCategoryFilters.includes(tag));
+
+    const hasRarityTag = tags.some((tag) => ["SECRET", "SUPER RARE", "SUPER SECRET RARE"].includes(tag));
+    const rarityMatch = activeRarityFilters.includes("All") ||
+      (activeRarityFilters.includes("NORMAL") && !hasRarityTag) ||
+      tags.some((tag) => activeRarityFilters.includes(tag));
+
+    const folderMatch = activeFolderFilters.includes("All") || activeFolderFilters.includes(card.dataset.folder);
+    card.style.display = categoryMatch && rarityMatch && folderMatch ? "inline-flex" : "none";
+  });
+
+  document.querySelectorAll(".group-section").forEach((section) => {
+    const anyVisible = Array.from(section.querySelectorAll(".template-card")).some((card) => card.style.display !== "none");
+    section.style.display = anyVisible ? "block" : "none";
+  });
+
+  updatePageCount();
+}
+
+function init() {
+  createCategoryFilterButtons();
+  createRarityFilterButtons();
+  createFolderFilterButtons();
+  sourceGroups.forEach((group) => {
+    const section = document.createElement("section");
+    section.className = "group-section";
+
+    const heading = document.createElement("h2");
+    heading.className = "group-title";
+    heading.textContent = group.title;
+
+    const row = document.createElement("div");
+    row.className = "template-grid";
+
+    group.files.forEach((fileName) => {
+      const src = `./source/${group.folder}/${fileName}`;
+      const alt = fileName.replace(/\.[^.]+$/, "").replace(/_/g, " ");
+      const card = createTemplateCard(src, alt, fileName);
+      card.dataset.folder = group.title;
+      row.appendChild(card);
+    });
+
+    section.appendChild(heading);
+    section.appendChild(row);
+    galleryGroups.appendChild(section);
+  });
+
+  updatePageCount();
+
+  const firstCard = document.querySelector(".template-card");
+  if (firstCard) {
+    firstCard.classList.add("active");
+  }
+  const firstCategoryFilter = filterBar.querySelector(".filter-button");
+  if (firstCategoryFilter) {
+    firstCategoryFilter.classList.add("active");
+  }
+
+  const firstRarityFilter = rarityFilterBar.querySelector(".filter-button");
+  if (firstRarityFilter) {
+    firstRarityFilter.classList.add("active");
+  }
+
+  const firstFolderFilter = folderFilterBar.querySelector(".filter-button");
+  if (firstFolderFilter) {
+    firstFolderFilter.classList.add("active");
+  }
+
+}
+
+init();
